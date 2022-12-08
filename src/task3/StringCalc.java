d73bb107f639861da5bba26a39c1293f412841d3
o-wael <omarwaelmahmoud@gmail.com>
2022-12-08 23:02:13 +0200

    ✅ made `'addUpToTwoNumbersestt' pass

--- src/task3/StringCalc.java
+++ src/task3/StringCalc.java
@@ -2,4 +2,10 @@ package task3;
 
 public class StringCalc {
 
+	public int Add(String string) {
+		String[] nums = new String[2];
+		nums = string.split(",");
+		return Integer.parseInt(nums[0])+Integer.parseInt(nums[1]);
+	}
+
 }
