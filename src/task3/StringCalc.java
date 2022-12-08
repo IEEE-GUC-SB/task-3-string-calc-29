d4afb5910e671ab1b895ef81f749ddb92757a27a
o-wael <omarwaelmahmoud@gmail.com>
2022-12-08 23:31:29 +0200

    ✅ made  'useDifferentDelimter' pass

--- src/task3/StringCalc.java
+++ src/task3/StringCalc.java
@@ -3,7 +3,12 @@ package task3;
 public class StringCalc {
 
 	public int Add(String string) {
+		String[] delimters = string.split("\n");
 		String[] nums = string.split("[\n|,]");
+		if(string.charAt(0)=='/') {
+			String[] del = delimters[0].split("/");
+			nums = delimters[1].split(del[del.length-1]);
+		}
 		
 		int sum = 0;
 		for(int i=0;i<nums.length;i++) {
